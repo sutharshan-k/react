95d8a31fe0f01142907d7fcbf774c2fe67c5af1b
Sutharshan <sutharshank107@gmail.com>
2025-06-23 11:54:52 +0530

    day777

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+function About() {
+    <>
+        <h1>About Us</h1>
+        <h1>Hi, I am Tharunkumr form Salem</h1>
+
+    </>
+}
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+function Contact() {
+    return (
+        <div>
+            <h1>Contact:</h1>
+            <p>Phone</p>
+        </div>
+    );
+}
+export default Contact;
+
+
+
